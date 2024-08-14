3f23d3f18ee0bc450eb804a99e1d456ba36d69a9
RaonJena99 <yejunjaj@gmail.com>
2024-08-14 17:45:38 +0900

    fix: git 업로드 방지

--- firebase.js
+++ firebase.js
@@ -1,15 +1,20 @@
 import { initializeApp } from "firebase/app";
 import { getDatabase } from "firebase/database";
 import { getStorage } from "firebase/storage";
+import { getAnalytics } from "firebase/analytics";
 
 // TODO: Replace the following with your app's Firebase project configuration
 // See: https://firebase.google.com/docs/web/learn-more#config-object
 const firebaseConfig = {
-  // ...
-  // The value of `databaseURL` depends on the location of the database
+  apiKey: "AIzaSyC8_u_S-lKWrlRS_MHCnP2whAD8NGFc75c",
+  authDomain: "carrot-clone-6f987.firebaseapp.com",
   databaseURL:
-    "https://carrot-clone-6f987-default-rtdb.asia-southeast1.firebasedatabase.app/",
-  storageBucket: "gs://carrot-clone-6f987.appspot.com",
+    "https://carrot-clone-6f987-default-rtdb.asia-southeast1.firebasedatabase.app",
+  projectId: "carrot-clone-6f987",
+  storageBucket: "carrot-clone-6f987.appspot.com",
+  messagingSenderId: "168978113538",
+  appId: "1:168978113538:web:7ad9f924e8a06d15de331b",
+  measurementId: "G-035CBC23ZW",
 };
 
 // Initialize Firebase
@@ -18,3 +23,4 @@ const app = initializeApp(firebaseConfig);
 // Initialize Realtime Database and get a reference to the service
 const database = getDatabase(app);
 const storage = getStorage(app);
+const analytics = getAnalytics(app);
